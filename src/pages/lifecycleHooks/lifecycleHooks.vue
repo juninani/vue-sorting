ca71c4f77bbc77b44d2f8a850a904205626dfea8
LEEJUNHEE <juninani123@gmail.com>
2022-11-11 16:59:58 +0900

    fix

--- src/pages/lifecycleHooks/lifecycleHooks.vue
+++ src/pages/lifecycleHooks/lifecycleHooks.vue
@@ -29,7 +29,7 @@ const buttonHandler = () => {
   <main class="life-wrapper">
     <div class="life-form">
       <div class="life-title">Lifecylce test</div>
-      {{/** 반응형 버튼 텍스트 언래핑 */}}
+      <!--반응형 버튼 텍스트 언래핑-->
       <div class="test-section">
         <div class="button-area">
           <button @click="buttonHandler">button</button>
@@ -38,7 +38,7 @@ const buttonHandler = () => {
         <div class="count">{{ ynText }}</div>
       </div>
 
-      {{/** 언래핑은 ref가 템플릿 렌더 컨텍스트의 최상위 속성인 경우 적용 */}}
+      <!--언래핑은 ref가 템플릿 렌더 컨텍스트의 최상위 속성인 경우 적용 -->
       <div class="test-section">
         <div class="text">{{ obj.foo + 1 }}</div>
         <div class="text">{{ foo + 1 }}</div>
